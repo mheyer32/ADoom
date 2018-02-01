8fd1c547a9aa78a2a41fb2e2522df90e042045e6
mheyer <sonode@gmx.de>
2018-02-01 10:02:39 -0800

    Use a different fd2pragma option for DoomSnd.h
    This apparently produces less code and thus won't overflow our relocation
    tables during linking.

--- adoom_src/DoomSnd.h
+++ adoom_src/DoomSnd.h
@@ -5,43 +5,131 @@
 #define CLIB_DOOMSND_PROTOS_H
 #endif
 
-#ifndef __INLINE_MACROS_H
-#include <inline/macros.h>
-#endif
-
 #include <exec/types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #ifndef DOOMSND_BASE_NAME
 #define DOOMSND_BASE_NAME DoomSndBase
 #endif
 
-#define Sfx_SetVol(vol) LP1NR(0x1e, Sfx_SetVol, int, vol, d0, , DOOMSND_BASE_NAME)
+#define Sfx_SetVol(vol) ({ \
+  int _Sfx_SetVol_vol = (vol); \
+  ({ \
+  register char * _Sfx_SetVol__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Sfx_SetVol__bn - 30))(_Sfx_SetVol__bn, _Sfx_SetVol_vol); \
+});})
 
-#define Sfx_Start(wave, cnum, step, vol, sep, length)                                                               \
-    LP6NR(0x24, Sfx_Start, char *, wave, a0, int, cnum, d0, int, step, d1, int, vol, d2, int, sep, d3, int, length, \
-          d4, , DOOMSND_BASE_NAME)
+#define Sfx_Start(wave, cnum, step, vol, sep, length) ({ \
+  char * _Sfx_Start_wave = (wave); \
+  int _Sfx_Start_cnum = (cnum); \
+  int _Sfx_Start_step = (step); \
+  int _Sfx_Start_vol = (vol); \
+  int _Sfx_Start_sep = (sep); \
+  int _Sfx_Start_length = (length); \
+  ({ \
+  register char * _Sfx_Start__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), char * __asm("a0"), int __asm("d0"), int __asm("d1"), int __asm("d2"), int __asm("d3"), int __asm("d4"))) \
+  (_Sfx_Start__bn - 36))(_Sfx_Start__bn, _Sfx_Start_wave, _Sfx_Start_cnum, _Sfx_Start_step, _Sfx_Start_vol, _Sfx_Start_sep, _Sfx_Start_length); \
+});})
 
-#define Sfx_Update(cnum, step, vol, sep) \
-    LP4NR(0x2a, Sfx_Update, int, cnum, d0, int, step, d1, int, vol, d2, int, sep, d3, , DOOMSND_BASE_NAME)
+#define Sfx_Update(cnum, step, vol, sep) ({ \
+  int _Sfx_Update_cnum = (cnum); \
+  int _Sfx_Update_step = (step); \
+  int _Sfx_Update_vol = (vol); \
+  int _Sfx_Update_sep = (sep); \
+  ({ \
+  register char * _Sfx_Update__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"), int __asm("d1"), int __asm("d2"), int __asm("d3"))) \
+  (_Sfx_Update__bn - 42))(_Sfx_Update__bn, _Sfx_Update_cnum, _Sfx_Update_step, _Sfx_Update_vol, _Sfx_Update_sep); \
+});})
 
-#define Sfx_Stop(cnum) LP1NR(0x30, Sfx_Stop, int, cnum, d0, , DOOMSND_BASE_NAME)
+#define Sfx_Stop(cnum) ({ \
+  int _Sfx_Stop_cnum = (cnum); \
+  ({ \
+  register char * _Sfx_Stop__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Sfx_Stop__bn - 48))(_Sfx_Stop__bn, _Sfx_Stop_cnum); \
+});})
 
-#define Sfx_Done(cnum) LP1(0x36, int, Sfx_Done, int, cnum, d0, , DOOMSND_BASE_NAME)
+#define Sfx_Done(cnum) ({ \
+  int _Sfx_Done_cnum = (cnum); \
+  ({ \
+  register char * _Sfx_Done__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((int (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Sfx_Done__bn - 54))(_Sfx_Done__bn, _Sfx_Done_cnum); \
+});})
 
-#define Mus_SetVol(vol) LP1NR(0x3c, Mus_SetVol, int, vol, d0, , DOOMSND_BASE_NAME)
+#define Mus_SetVol(vol) ({ \
+  int _Mus_SetVol_vol = (vol); \
+  ({ \
+  register char * _Mus_SetVol__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Mus_SetVol__bn - 60))(_Mus_SetVol__bn, _Mus_SetVol_vol); \
+});})
 
-#define Mus_Register(musdata) LP1(0x42, int, Mus_Register, void *, musdata, a0, , DOOMSND_BASE_NAME)
+#define Mus_Register(musdata) ({ \
+  void * _Mus_Register_musdata = (musdata); \
+  ({ \
+  register char * _Mus_Register__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((int (*)(char * __asm("a6"), void * __asm("a0"))) \
+  (_Mus_Register__bn - 66))(_Mus_Register__bn, _Mus_Register_musdata); \
+});})
 
-#define Mus_Unregister(handle) LP1NR(0x48, Mus_Unregister, int, handle, d0, , DOOMSND_BASE_NAME)
+#define Mus_Unregister(handle) ({ \
+  int _Mus_Unregister_handle = (handle); \
+  ({ \
+  register char * _Mus_Unregister__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Mus_Unregister__bn - 72))(_Mus_Unregister__bn, _Mus_Unregister_handle); \
+});})
 
-#define Mus_Play(handle, looping) LP2NR(0x4e, Mus_Play, int, handle, d0, int, looping, d1, , DOOMSND_BASE_NAME)
+#define Mus_Play(handle, looping) ({ \
+  int _Mus_Play_handle = (handle); \
+  int _Mus_Play_looping = (looping); \
+  ({ \
+  register char * _Mus_Play__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"), int __asm("d1"))) \
+  (_Mus_Play__bn - 78))(_Mus_Play__bn, _Mus_Play_handle, _Mus_Play_looping); \
+});})
 
-#define Mus_Stop(handle) LP1NR(0x54, Mus_Stop, int, handle, d0, , DOOMSND_BASE_NAME)
+#define Mus_Stop(handle) ({ \
+  int _Mus_Stop_handle = (handle); \
+  ({ \
+  register char * _Mus_Stop__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Mus_Stop__bn - 84))(_Mus_Stop__bn, _Mus_Stop_handle); \
+});})
 
-#define Mus_Pause(handle) LP1NR(0x5a, Mus_Pause, int, handle, d0, , DOOMSND_BASE_NAME)
+#define Mus_Pause(handle) ({ \
+  int _Mus_Pause_handle = (handle); \
+  ({ \
+  register char * _Mus_Pause__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Mus_Pause__bn - 90))(_Mus_Pause__bn, _Mus_Pause_handle); \
+});})
 
-#define Mus_Resume(handle) LP1NR(0x60, Mus_Resume, int, handle, d0, , DOOMSND_BASE_NAME)
+#define Mus_Resume(handle) ({ \
+  int _Mus_Resume_handle = (handle); \
+  ({ \
+  register char * _Mus_Resume__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((void (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Mus_Resume__bn - 96))(_Mus_Resume__bn, _Mus_Resume_handle); \
+});})
 
-#define Mus_Done(handle) LP1(0x66, int, Mus_Done, int, handle, d0, , DOOMSND_BASE_NAME)
+#define Mus_Done(handle) ({ \
+  int _Mus_Done_handle = (handle); \
+  ({ \
+  register char * _Mus_Done__bn __asm("a6") = (char *) (DOOMSND_BASE_NAME);\
+  ((int (*)(char * __asm("a6"), int __asm("d0"))) \
+  (_Mus_Done__bn - 102))(_Mus_Done__bn, _Mus_Done_handle); \
+});})
 
+
+#ifdef __cplusplus
+}
+#endif
 #endif /*  _INLINE_DOOMSND_H  */
